722706815cd72a8d0fbdca4feae7dc76dc8a5c8f
Ger Hobbelt <ger@hobbelt.com>
2023-06-20 13:28:14 +0200

    delete R-specific files before merge & remix

--- tests/testthat.R
+++
@@ -1,6 +0,0 @@
-if(require("testthat")) {
-    library(testthat)
-    library(rbibutils)
-    test_check("rbibutils")
-} else
-    warning("package 'testthat' required for the tests in 'rbibutils'")
